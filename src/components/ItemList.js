ef282e8d4e94311ff2b40843afc86132135ee84a
lukmaan-b <lukmaan.butt@gmail.com>
2021-01-14 12:33:50 +0000

    Add: custom styles

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,21 +3,52 @@ import PropTypes from 'prop-types';
 import { Button, FormCheck, ListGroup } from 'react-bootstrap';
 
 const ItemList = ({ list, handleToggleItem, handleRemoveItem }) => {
-  console.log(list);
+  const checkedStyles = {
+    container: {
+      backgroundColor: '#8b7c81',
+    },
+    text: {
+      textDecoration: 'line-through',
+    },
+  };
+
+  const nonCheckedStyles = {
+    container: {
+      backgroundColor: '#a3989c',
+    },
+    text: {
+      textDecoration: 'none',
+    },
+  };
+
   const listItemComponents = list.map((i) => (
-    <ListGroup.Item className="d-flex" key={i.id}>
+    <ListGroup.Item
+      className="d-flex"
+      key={i.id}
+      style={
+        i.isChecked
+          ? { ...checkedStyles.container }
+          : nonCheckedStyles.container
+      }
+    >
       <FormCheck
         inline
         checked={i.isChecked}
         onChange={({ target: { checked } }) => handleToggleItem(i.id, checked)}
       />
-      <span className="flex-grow-1 p-2">{i.body}</span>
+      <span
+        style={i.isChecked ? { ...checkedStyles.text } : nonCheckedStyles.text}
+        className="flex-grow-1 p-2"
+      >
+        {i.body}
+      </span>
       <Button variant="danger" onClick={() => handleRemoveItem(i.id)}>
-        🗑️
+        X
       </Button>
     </ListGroup.Item>
   ));
-  return list.length && <ListGroup>{listItemComponents}</ListGroup>;
+
+  return list.length > 0 && <ListGroup>{listItemComponents}</ListGroup>;
 };
 
 ItemList.propTypes = {
